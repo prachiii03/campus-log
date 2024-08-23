8b6a336818363d4d64472bd9f134aa036cc23bcb
manikerisaurabh <manikerisaurabh@gmail.com>
2024-08-24 00:14:23 +0530

    added migration command for production

---
+++ prisma/migrations/20240823184344_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Student" (
+    "rollNo" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "prn" INTEGER NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("rollNo")
+);
