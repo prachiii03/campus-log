b269ac5888fe799336c4723ce8aa84152dd06d3a
manikerisaurabh <manikerisaurabh@gmail.com>
2024-08-24 00:19:36 +0530

    we can start development

--- prisma/migrations/20240823184344_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Student" (
-    "rollNo" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "prn" INTEGER NOT NULL,
-
-    CONSTRAINT "Student_pkey" PRIMARY KEY ("rollNo")
-);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { addStudent } from "@/actions/actions";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
-      <form action={addStudent}>
-
-      <button >add data</button>
-      </form>
+      <strong>our code goes here </strong>
    </div>
   );
 }
