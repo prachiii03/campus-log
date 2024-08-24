48208d805747703be25772433f7793f9c02ca0af
Prachi Lad <prachilad061@gmail.com>
2024-08-24 11:16:47 +0530

    test git commands

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-      <strong>our code goes here from m prachi </strong>
+      <strong>our code goes here from m prachi from temp 2 </strong>
    </div>
   );
 }
