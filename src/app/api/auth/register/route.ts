c9731369073c43938a7c1e1c5e8df5c3314e91fe
manikerisaurabh <manikerisaurabh@gmail.com>
2024-08-29 21:03:34 +0530

    created register backend partially

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,5 +1,18 @@
-export default async function POST(req: Request) {
+import prisma from "@/lib/db";
+import { NextResponse } from "next/server"
+import { registerStudent } from "./studentRegisterService";
+
+export async function POST(req: Request) {
+    const data = await req.json();
+    console.log(data);
     try {
+
+        if (data.profession == 0) {
+            const res = await registerStudent(data);
+            return NextResponse.json(res);
+        }
+
+        return NextResponse.json({ message: "hi" })
     } catch (error) {
         console.log({ error })
     }
