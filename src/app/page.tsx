f00eebfbad548bff3e5c8c71205615b2a836908e
Amruta Jodagudri <144842080+amruta-Jodagudri@users.noreply.github.com>
2024-08-31 16:06:10 +0530

    implemented sidebar and dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
-      <strong>our code goes here from m prachi from temp 2 </strong>
+      {/* <strong>our code goes here from m prachi from temp 2 </strong> */}
    </div>
   );
 }
