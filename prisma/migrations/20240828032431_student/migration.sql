c48f77c14ab580275dd2df1702692ea7a763f29e
manikerisaurabh <manikerisaurabh@gmail.com>
2024-09-01 08:45:25 +0530

    connected to supabase and generated new schema

--- prisma/migrations/20240828032431_student/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Student" ALTER COLUMN "prn" SET DATA TYPE TEXT;
--- prisma/migrations/20240828180241_final_databse/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `is_registerd` to the `prn_table` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "prn_table" ADD COLUMN     "is_registerd" BOOLEAN NOT NULL;
