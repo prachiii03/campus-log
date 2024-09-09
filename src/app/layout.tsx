493022e4aecc2699fe01281854d03963e9cf5cee
manikerisaurabh <manikerisaurabh@gmail.com>
2024-09-09 09:32:32 +0530

    Revert "Merge pull request #10 from amruta-Jodagudri/dashboard-component"
    This reverts commit b164b78ae22f904531c9b118bbf9ae4017eee602, reversing
    changes made to 285bef338fad1e3a140db17e44d638fc6c995219.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
 import "./globals.css";
-import Navbar from "./Navbar/Navbar";
-import { SidebarDemo } from "./Sidebar/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,13 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-      </head>
-      <body className={inter.className}>
-        <Navbar/>
-        <SidebarDemo />
-        {children}
-      </body>
+
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
